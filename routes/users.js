c0f3835ef13734d7017779fc74a6dd551beb70ad
Jake Ashton <4ashtj48@solent.ac.uk>
2021-01-08 14:27:46 +0000

    update users.js

--- routes/users.js
+++ routes/users.js
@@ -11,12 +11,25 @@ router.get('/login',(req,res) =>res.render('Login'));
 router.get('/register',(req,res) =>res.render('Register'));
 
 
-
 //Register Handle
 //handles the post request
 router.post('/register', (req,res) =>{
-console.log(req.body)
-res.send('hello');
+const {name, email, password, password2} = req.body;
+
+let errors = [];
+
+//validation- check fields
+if(!name || !email || !password || !password2){
+    errors.push({msg: 'Please fill in all fields'});
+}
+//validation- check passwords match
+if(password !== password2){
+   errors.push({ mesg: 'Passwords do not match'}) 
+}
+// 6 character min
+if(password.length < 6){
+    errors.push({msg : 'Password should be at least 6 chars'})
+}
 
 });
 
